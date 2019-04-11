59dfb6df88de921bd8dc3297aa5cc42358e00832
Marcus Farkas <toothlessgear@finitebox.com>
2019-04-12 00:10:51 +0200

    Add support for ACR tags metadata

--- registry/tags.go
+++ registry/tags.go
@@ -1,5 +1,7 @@
 package registry
 
+import "time"
+
 type tagsResponse struct {
 	Tags []string `json:"tags"`
 }
@@ -22,4 +24,37 @@ func (registry *Registry) Tags(repository string) (tags []string, err error) {
 			return nil, err
 		}
 	}
+}
+
+type acrTagsResponse struct {
+	Registry  string   `json:"registry"`
+	ImageName string   `json:"imageName"`
+	Tags      []acrTag `json:"tags"`
+}
+
+type acrTag struct {
+	Name           string    `json:"name"`
+	LastUpdateTime time.Time `json:"lastUpdateTime"`
+	Digest         string    `json:"digest"`
+	CreatedTime    time.Time `json:"createdTime"`
+	Signed         bool      `json:"signed"`
+}
+
+type changeableAttributes struct {
+	DeleteEnabled bool `json:"deleteEnabled"`
+	WriteEnabled  bool `json:"writeEnabled"`
+	ReadEnabled   bool `json:"readEnabled"`
+	ListEnabled   bool `json:"listEnabled"`
+}
+
+func (registry *Registry) AcrTags(repository string) (tags *acrTagsResponse, err error) {
+	url := registry.url("/acr/v1/%s/_tags", repository)
+
+	var response acrTagsResponse
+	registry.Logf("registry.tags url=%s repository=%s", url, repository)
+	err = registry.getJson(url, &response)
+	if err != nil {
+		return nil, err
+	}
+	return &response, nil
 }
